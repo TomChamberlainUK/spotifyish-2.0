bdf9815bf3adf2f28a51e37b5d3c4846cabb8b39
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-02 17:14:01 +0100

    Added semicolons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
@@ -14,4 +14,4 @@ const Home: NextPage = () => {
   );
 }
 
-export default Home
+export default Home;
