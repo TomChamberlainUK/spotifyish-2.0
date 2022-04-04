8d9b8b9d4d8776e23a3f226dce87526e30705d90
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-04 21:56:23 +0100

    Added accessToken to session object

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -7,5 +7,17 @@ export default NextAuth({
       clientId: process.env.SPOTIFY_CLIENT_ID ?? '',
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET ?? ''
     })
-  ]
-})
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      return session;
+    }
+  }
+});
