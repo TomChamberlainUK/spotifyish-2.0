e2ef734e8151d5125cd8739050ed59360cfef7f2
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-04 21:41:36 +0100

    Implemented auth check for protected pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import type { NextComponentType } from 'next';
+import type { ReactNode } from 'react';
+import { SessionProvider, useSession } from 'next-auth/react';
+
+
+type CustomAppProps = AppProps & {
+  Component: NextComponentType & {
+    auth?: boolean
+  }
+}
 
 function MyApp({
   Component,
@@ -8,12 +17,32 @@ function MyApp({
     session,
     ...pageProps
   }
-}: AppProps) {
+}: CustomAppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      {
+        Component.auth
+          ? (<Auth>
+              <Component {...pageProps} />
+            </Auth>)
+          : <Component {...pageProps} />
+      }
+
     </SessionProvider>
   );
+}
+
+type AuthProps = {
+  children: ReactNode
+}
+
+function Auth({ children }: AuthProps) {
+  const { data: session, status } = useSession({ required: true });
+  const isUser = session?.user;
+  if (isUser) {
+    return <>{children}</>;
+  }
+  return <div>Loading...</div>;
 }
 
 export default MyApp;
