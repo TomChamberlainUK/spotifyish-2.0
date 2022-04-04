7bfe04d62b8eccd7cbf3569264c2ec4c50d86b35
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-04 14:29:22 +0100

    Added sidebar to layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import { useState } from 'react';
 import styles from './Layout.module.scss';
 
 type Props = {
@@ -6,11 +7,24 @@ type Props = {
 }
 
 function Layout({ children }: Props) {
+
+  const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
+  function toggleSidebar() {
+    setSidebarIsOpen(!sidebarIsOpen);
+  }
+
   return (
     <div className={styles.container}>
       <header className={styles.header}>
+        <button onClick={toggleSidebar}>
+          menu
+        </button>
         <h1 className={styles.pageTitle}>Spotifyish</h1>
       </header>
+      <nav className={`${styles.sidebar} ${sidebarIsOpen ? styles.isOpen : ''}`}>
+        Menu
+      </nav>
       <main className={styles.main}>
         {children}
       </main>
