39098b98db8b9086958ea97beeb10a2b805e1b9a
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-04 22:04:55 +0100

    Built hook to get recently played tracks and artists from spotify api

---
+++ hooks/useGetRecentlyPlayed.tsx
@@ -0,0 +1,102 @@
+import type { Session } from 'next-auth';
+import { useEffect, useState } from 'react';
+import useSWR from 'swr';
+
+type Artist = {
+  id: string;
+  name: string;
+}
+
+type Track = {
+  id: string;
+  name: string;
+  artist: string;
+  artistId: string;
+  album: string;
+  imageUrl: string;
+}
+
+// Prepare fetcher for useSWR
+async function fetcher(url: string, accessToken: string) {
+  
+  // Fetch data
+  const response = await fetch(url, {
+    headers: {
+      "Authorization": `Bearer ${accessToken}`
+    }
+  });
+
+  // Handle errors
+  if (!response.ok) {
+    const { error } = await response.json();
+    throw new Error(`An error occurred whilst fetching data: (${error.status}) ${error.message}`);
+  }
+
+  // Parse and return data
+  const data = await response.json();
+  return data;
+
+}
+
+export default function useGetRecentlyPlayed(session: Session | null) {
+
+  // Init state for tracks and artists
+  const [tracks, setTracks] = useState<Track[]>([]);
+  const [artists, setArtists] = useState<Artist[]>([]);
+
+  // Get recently played tracks data
+  const { data, error } = useSWR(
+    session
+      ? ['https://api.spotify.com/v1/me/player/recently-played', session.accessToken]
+      : null,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (!error && data) {
+
+      // Init maps to efficiently parse track/artist data and prevent duplicates with O(n) time complexity
+      const uniqueTracks = new Map<string, Track>();
+      const uniqueArtists = new Map<string, Artist>();
+  
+      // Loop through each track from the response
+      for (const { track } of data.items) {
+        const artist = track.artists[0];
+  
+        // If the track is not already mapped, add it
+        if (!uniqueTracks.has(track.id)) {
+          console.log(track);
+          uniqueTracks.set(track.id, {
+            id: track.id,
+            name: track.name,
+            artist: artist.name,
+            artistId: artist.id,
+            album: track.album.name,
+            imageUrl: track.album.images[0].url,
+          });
+        }
+        
+        // If the artist is not already mapped, add them
+        if (!uniqueArtists.has(artist.id)) {
+          uniqueArtists.set(artist.id, {
+            id: artist.id,
+            name: artist.name
+          });
+        }
+      
+      }
+  
+      // Update artists and tracks state
+      setTracks(Array.from(uniqueTracks.values()));
+      setArtists(Array.from(uniqueArtists.values()));
+  
+    }
+  }, [error, data]);
+
+  return {
+    tracks,
+    artists,
+    error,
+    isLoading: !data && !error
+  }
+}
