505c51de0c79ac8eecbf14a1ad0ef8dd2bb74fa0
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-04 14:05:10 +0100

    Removed Spotify domain from next image processing because host is dynamic using cdn

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['scontent-cdg2-1.xx.fbcdn.net']
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
