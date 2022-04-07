b129f0f61b4167fdaa4c4eee695fc7e91bffe340
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-07 17:57:11 +0100

    Added bare bones content and styling

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,9 @@
 import type { ProtectedNextPage } from '@tsTypes/ProtectedNextPage';
+import Head from 'next/head';
 import Layout from '@components/Layout/Layout';
 import LoadingThrobber from '@components/LoadingThrobber/LoadingThrobber';
 import useGetUser from '@hooks/useGetUser';
+import styles from '@styles/profile.module.scss';
 
 const Profile: ProtectedNextPage = () => {
 
@@ -10,30 +12,39 @@ const Profile: ProtectedNextPage = () => {
 
   return (
     <Layout>
-      <h1>Profile</h1>
-      {
-        isLoading
-          ? <LoadingThrobber />
-          : error
-            ? <p>An error has occurred, please refresh your browser.</p>
-            : (
-              <>
-                <img
-                  src={user?.imageUrl ?? ''}
-                  alt={`profile picture for ${user?.name ?? user?.email ?? 'unknown user'}`}
-                />
-                <p>
-                  {user?.name ?? '(Username not found)'}
-                </p>
-                <p>
-                  {user?.email ?? '(Email not found)'}
-                </p>
-                <p>
-                  Followers: {user?.followers ?? '(Followers not found)'}
-                </p>
-              </>
-            )
-      }
+      <Head>
+        <title>{user?.name ?? 'Profile'} | Spotifyish</title>
+        <meta name="description" content="View user profile information." />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          {
+            isLoading
+              ? <LoadingThrobber />
+              : error
+                ? <p>An error has occurred, please refresh your browser.</p>
+                : (
+                  <>
+                    <img
+                      src={user?.imageUrl ?? ''}
+                      alt={`profile picture for ${user?.name ?? user?.email ?? 'unknown user'}`}
+                      className={styles.profilePicture}
+                    />
+                    <h1 className={styles.userNameText}>
+                      {user?.name ?? '(Name not found)'}
+                    </h1>
+                    <p className={styles.userEmailText}>
+                      {user?.email ?? '(Email not found)'}
+                    </p>
+                    <p className={styles.paragraph}>
+                      Followers: {user?.followers ?? '(Followers not found)'}
+                    </p>
+                  </>
+                )
+          }
+        </div>
+      </div>
     </Layout>
   );
 }
