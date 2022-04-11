25a5b0b5411f4d98ed9c88e936a9b11991e4320f
Tom Chamberlain <tom_chamberlain_uk@hotmail.co.uk>
2022-04-11 12:17:46 +0100

    Added MusicPlayerContext provider and state for currently playing track

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import '../styles/global.scss';
 import type { AppProps } from 'next/app';
 import type { NextComponentType } from 'next';
+import { useState } from 'react';
 import { SessionProvider } from 'next-auth/react';
 import Auth from '@components/Auth/Auth';
+import MusicPlayerContext from '@context/MusicPlayer/MusicPlayerContext';
 
 type Props = AppProps & {
   Component: NextComponentType & {
@@ -17,16 +19,25 @@ function MyApp({
     ...pageProps
   }
 }: Props) {
+
+  const [currentlyPlaying, setCurrentlyPlaying] = useState('');
+
   return (
     <SessionProvider session={session}>
-      {
-        Component.auth
-          ? (<Auth>
-              <Component {...pageProps} />
-            </Auth>)
-          : <Component {...pageProps} />
-      }
-
+      <MusicPlayerContext.Provider
+        value={{
+          currentlyPlaying,
+          setCurrentlyPlaying
+        }}
+      >
+        {
+          Component.auth
+            ? (<Auth>
+                <Component {...pageProps} />
+              </Auth>)
+            : <Component {...pageProps} />
+        }
+      </MusicPlayerContext.Provider>
     </SessionProvider>
   );
 }
